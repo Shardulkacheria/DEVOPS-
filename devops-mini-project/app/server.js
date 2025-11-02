27f59a907973b674560fd6743dd8be0402f0c77c
Shardulkacheria <141412151+Shardulkacheria@users.noreply.github.com>
2025-11-03 01:44:03 +0530

    jenkins

--- devops-mini-project/app/server.js
+++ devops-mini-project/app/server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps 🚀 – CI/CD Success!");
+  res.send("<h1>Hello DevOps 🚀</h1>");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+app.listen(3000, () => console.log("App running on port 5000"));
